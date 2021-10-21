b4c4ffe874ae6dea722c580df19ea2a967ae4c70
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-10-20 20:45:24 -0700

    autocomplete

---
+++ exercise/search-compendium.php
@@ -0,0 +1,28 @@
+<?php
+
+  require_once("../private/dbinfo.inc.php");
+
+  $link = mysqli_connect($host, $username, $password, $db_name);
+
+  if(mysqli_connect_error()){
+    echo "Failed to connect to database.";
+  } else {
+    echo "Successfully connected to database.";
+  }
+
+  $query = "SELECT specificActivities FROM activity limit 10";
+  $result = $conn->query($query);
+
+  if($result->num_rows > 0){
+
+      while($row = $result->fetch_assoc()) {
+        echo $row["book title"]."<br>";
+      }
+
+  } else {
+
+    echo "0 results";
+
+  }
+  $conn->close();
+?>
