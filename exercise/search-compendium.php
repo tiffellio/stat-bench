aff83f702aba90980f5e8348cbb1e0fe5df20e32
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-10-25 18:28:49 -0700

    remove value

--- exercise/search-compendium.php
+++ exercise/search-compendium.php
@@ -39,7 +39,7 @@
 
    $response = array();
    while($row = mysqli_fetch_array($result) ){
-     $response[] = array("value"=>$row['id'],"label"=>$row['name']);
+     $response[] = array("label"=>$row['name']);
    }
 
    echo json_encode($response);
