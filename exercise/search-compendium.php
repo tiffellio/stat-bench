817f4eb8ad5c0856b7012e0478224e73138716b6
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-10-27 21:14:29 -0700

    .

--- exercise/search-compendium.php
+++ exercise/search-compendium.php
@@ -2,52 +2,34 @@
 
   require_once("../private/dbinfo.inc.php");
 
-  $db = new mysqli($host, $username, $password, $db_name);
+  $conn = mysqli_connect($host, $username, $password, $db_name) or die($conn);
 
-  if($db->connect_error){
-    echo "Failed to connect to database.";
-  } else {
-    echo "Successfully connected to database.";
+  function get_city($conn , $term){
+
+     $query = "SELECT * FROM activity WHERE specificActivities LIKE '%".$term."%' limit 5";
+
+     $result = mysqli_query($conn, $query);
+
+     $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+     return $data;
   }
 
-// get search term
-  $searchTerm = $_GET['term'];
 
-  // fetch matched data from the database
-  $query = $db->query("SELECT * FROM activity WHERE specificActivities LIKE '%".$searchTerm."%' limit 5");
+  if (isset($_GET['term'])) {
 
+     $getCity = get_city($conn, $_GET['term']);
 
-  // generate array with activity data
-  $activityData = array();
-  if($query->num_rows > 0) {
-    while($row = $query->fetch_assoc()){
-      $data['id'] = $row['metCode'];
-      $data['value'] = $row['specificActivities'];
-      array_push($activityData, $data);
-    }
-  } else {
-    echo "no data";
+     $cityList = array();
+
+     foreach($getCity as $city){
+
+     $cityList[] = $city['specificActivities'];
+
+   }
+
+   echo json_encode($cityList);
+
   }
-
-echo json_encode($activityData);
-/*
-// Get search term
-$searchTerm = $_GET['term'];
-
-// Fetch matched data from the database
-$query = $db->query("SELECT * FROM activity WHERE specificActivities LIKE '%".$searchTerm."%' AND status = 1 ORDER BY skill ASC");
-
-// Generate array with skills data
-$skillData = array();
-if($query->num_rows > 0){
-    while($row = $query->fetch_assoc()){
-        $data['id'] = $row['id'];
-        $data['value'] = $row['skill'];
-        array_push($skillData, $data);
-    }
-}
-
-// Return results as json encoded array
-echo json_encode($skillData);
-*/
+  
 ?>
