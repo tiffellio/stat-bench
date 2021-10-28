0f9a6b8830c6b443b0ce7a1337aa2c6475d3fb21
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-10-27 21:38:00 -0700

    finished autocomplete

--- exercise/search-compendium.php
+++ exercise/search-compendium.php
@@ -4,13 +4,13 @@
 
   $conn = mysqli_connect($host, $username, $password, $db_name) or die($conn);
 
-  function get_city($conn , $term){
+  function get_activity($conn , $term){
 
      $query = "SELECT * FROM activity WHERE specificActivities LIKE '%".$term."%' limit 5";
 
      $result = mysqli_query($conn, $query);
 
-     $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+     $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
      return $data;
   }
@@ -18,18 +18,18 @@
 
   if (isset($_GET['term'])) {
 
-     $getCity = get_city($conn, $_GET['term']);
+     $getActivity = get_activity($conn, $_GET['term']);
 
-     $cityList = array();
+     $activityList = array();
 
-     foreach($getCity as $city){
+     foreach($getActivity as $activity){
 
-     $cityList[] = $city['specificActivities'];
+     $activityList[] = $activity['specificActivities'];
 
    }
 
-   echo json_encode($cityList);
+   echo json_encode($activityList);
 
   }
-  
+
 ?>
