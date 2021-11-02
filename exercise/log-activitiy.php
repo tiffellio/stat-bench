7034b97eb2261186beb973d7e15356ffd4768a6c
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-11-01 18:55:28 -0700

    Create log-activitiy.php

---
+++ exercise/log-activitiy.php
@@ -0,0 +1,15 @@
+
+<?php
+require_once("../private/dbinfo.inc.php");
+
+$conn = mysqli_connect($host, $username, $password, $db_name) or die($conn);
+$res = mysqli_query($conn, "SELECT * FROM `activity` WHERE `specificActivities` = 'bicycling, mountain, uphill, vigorous'") or die( mysqli_error($conn));
+$arr = mysqli_fetch_row($res);
+
+echo $arr[0];  // name
+echo $arr[1];  // city
+echo $arr[2];  // country
+
+
+
+?>
