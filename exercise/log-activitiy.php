8b21430e6d7074f525435e61c4b20926d832a7e7
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-11-01 18:59:46 -0700

    Update log-activitiy.php

--- exercise/log-activitiy.php
+++ exercise/log-activitiy.php
@@ -3,7 +3,7 @@
 require_once("../private/dbinfo.inc.php");
 
 $conn = mysqli_connect($host, $username, $password, $db_name) or die($conn);
-$res = mysqli_query($conn, "SELECT * FROM activity WHERE specificActivities = 'bicycling, mountain, uphill, vigorous' limit 1") or die( mysqli_error($conn));
+$res = mysqli_query($conn, "SELECT * FROM activity WHERE specificActivities = 'bicycling, mountain, uphill, vigorous' limit 1"); //or die( mysqli_error($conn));
 $arr = mysqli_fetch_row($res);
 
 echo $arr[0];  // name
