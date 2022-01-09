c2f62560af8d652029150ac6eda1339457358ce2
tiffellio <tiffanyelliott@outlook.com>
2022-01-09 14:13:20 -0800

    Activity, Sleep and Food logging to database complete

--- exercise/log-activity.php
+++ exercise/log-activity.php
@@ -1,22 +1,27 @@
 
 <?php
 
-  require_once("../private/dbinfo.inc.php");
+/**********TEMP VALS, REMEMBER TO UPDATE ************** */
+$ACTID = 9; // temporary global until account login is created
+$DATEID = 6; //will need to create date function when user logs in to call dateLog
 
-  $link = mysqli_connect($host, $username, $password, $db_name);
+require_once("../private/dbinfo.inc.php");
 
-  // check if connection is successful
-  if (mysqli_connect_error()){
+$link = mysqli_connect($host, $username, $password, $db_name);
 
-    die("There was a problem connecting to the MySQL database: ".mysqli_connect_error());
+// check if connection is successful
+if (mysqli_connect_error()){
 
-  } else {
+  die("There was a problem connecting to the MySQL database: ".mysqli_connect_error());
 
-    echo "Connection to new database was sucessful <br><br>";
+} else {
 
-  }
+  echo "Connection to new database was sucessful <br><br>";
 
-    if(isset($_POST['submit'])){
+}
+
+  if(isset($_POST['submit'])){
+    
     if(!empty($_POST['type']) &&
     !empty($_POST['duration'])){
 
@@ -42,23 +47,96 @@
 
         } else {
 
-            echo "Exercise exists in the compendium. <br>";
+            echo "Exercise exists in the compendium.Logging. <br>";
 
             print_r($new_array);
 
             // Store search results
             $metCode = $new_array[0]["metCode"];
 
-            // NEXT: ADD FUNCTION TO CALCULATE CALSBURNED FROM METCODE FORMULA
+            $mets = findMets($metCode, $link);
+            echo "Mets found are ".$mets;
 
+            $weightKg = findWeight($ACTID, $link);
+            echo "Weight found is ".$weightKg;
+
+            $calsBurned = metsToCals($weightKg, $mets, $duration);
+            echo "CalsBurned per ".$duration." minutes is ".$calsBurned;
+
+            //Add entry to activity log
+            $query = "INSERT INTO `stat_bench_3497853`.`activityLog` (`activityID`, `actID`, `dateID`, `metCode`, `duration`, `calsBurned`) VALUES (NULL, '$ACTID', '$DATEID', '$metCode', '$duration', '$calsBurned')";
+
+            $run = mysqli_query($link, $query) or die(mysqli_error($link));
+
+
+            // NEXT: OUTPUT SUCCESS MESSAFE ON EXERCISE.HTML USING AJAX
+
+
+
+            echo "Activity successfully logged into database";
         }
 
     } else {
 
-        echo "All fields are required. <br>";
-
-    }
+      echo "All fields are required. <br>";
 
   }
+
+}
+
+// Find the mets for a particular activity by looking up the corresponding met code in the activity table
+function findMets($metCode, $link){
+
+  $query = 'SELECT `mets` FROM `stat_bench_3497853`.`activity` WHERE `metCode` = "'.$metCode.'"';
+
+  $run = mysqli_query($link, $query) or die(mysqli_error($link));
+
+  while ($row = mysqli_fetch_assoc($run)){
+
+    $new_array[] = $row;
+
+  }
+
+  $mets = $new_array[0]["mets"];
+
+  return $mets;
+
+}
+
+// Find weight in KG of user with ACTID in the profile table
+function findWeight($ACTID, $link){
+
+  $query = 'SELECT `kg` FROM `stat_bench_3497853`.`profile` WHERE `actID` = "'.$ACTID.'"';
+
+  $run = mysqli_query($link,$query) or die(mysqli_error($link));
+  
+  while( $row = mysqli_fetch_assoc($run)){
+
+    $new_array[] = $row;
+
+  }
+
+  $weightKg = $new_array[0]["kg"];
+
+  return $weightKg;
+
+}
+
+// Use the met forumula to convert mets to calories burned
+function metsToCals($weightKg, $mets, $duration){
+
+  $o2ml = 3.5; // 3.5 mlof oxygen per kg
+
+  $metMinRate = 200;
+
+  $calsPerMin = $mets * $o2ml * $weightKg / $metMinRate;
+
+  $calsBurned = $calsPerMin * $duration;
+
+  $calsBurned = round($calsBurned);
+
+  return $calsBurned;
+
+}
 
 ?>
---
+++ sleep/log-sleep.php
@@ -0,0 +1,68 @@
+
+<?php
+
+/**********TEMP VALS, REMEMBER TO UPDATE ************** */
+$ACTID = 9; // temporary global until account login is created
+$DATEID = 6; //will need to create date function when user logs in to call dateLog
+
+require_once("../private/dbinfo.inc.php");
+
+$link = mysqli_connect($host, $username, $password, $db_name);
+
+if(isset($_POST['submit'])){
+    
+    if(!empty($_POST['sleep-quality'])){
+
+        if(empty($_POST['hours']) && empty($_POST['minutes'])) { // make sure both fields are set
+
+            echo "Please enter your time slept (cannot be 0)";
+
+        } else {
+
+            $hours = $_POST['hours'];
+            $minutes = $_POST['minutes'];
+            $sleepQuality = $_POST['sleep-quality'];
+
+            // convert to minutes
+            if ($hours > 0){
+
+                $totalMinutes = minutesAsleep($hours, $minutes);
+                echo "<br> Total minutes returned: ".$totalMinutes;
+
+            } else {
+
+                $totalMinutes = $minutes;
+
+            }
+
+
+            // log sleep in database
+            $query = "INSERT INTO `stat_bench_3497853`.`sleepLog`  (`sleepID`, `actID`, `dateID`, `sleepTime`, `quality`) VALUES (NULL, '$ACTID', '$DATEID', '$totalMinutes', '$sleepQuality')";
+            $run = mysqli_query ($link, $query) or die(mysqli_error($link));
+            echo "successfully added to sleepLog table";
+
+        }
+
+    }
+}
+
+
+function minutesAsleep($hours, $minutes){
+
+    $minsPerHr = 60;
+
+    $hrsToMin = $hours * $minsPerHr;
+
+    $totalMinutes = $hrsToMin + $minutes;
+
+    echo $totalMinutes;
+
+    return $totalMinutes;
+
+}
+
+
+
+
+
+?>
